35139a14074644e7c1a3ddd49d503c22f388e291
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-17 21:51:01 +0530

    Add files via upload

---
+++ strings_method.py
@@ -0,0 +1,42 @@
+# Python strings method
+a = "python".capitalize()
+print(a)
+
+a = "PYTHON".casefold()
+print(a)
+
+a = "python".count("p")
+print(a)
+
+a = "python".find("y")
+print(a)
+
+a = "python".index("yt")
+print(a)
+
+a = "python".isalnum()
+print(a)
+
+a = "python".isalpha()
+print(a)
+
+a = "python".isascii()
+print(a)
+
+a = "python".isdecimal()
+print(a)
+
+a = "python".isdigit()
+print(a)
+
+a = "python".isidentifier()
+print(a)
+
+a = "python".islower()
+print(a)
+
+a = "python".isnumeric()
+print(a)
+
+a = "python".isprintable()
+print(a)
